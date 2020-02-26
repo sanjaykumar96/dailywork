8f8c8475af20d98002a1cb83c836f7f187a42ee2
sanjay kumar patel <psanjaypatel2@gmail.com>
2020-02-25 17:34:26 -0800

    25febday21

---
+++ 25febday21/DaphneArray.java
@@ -0,0 +1,20 @@
+package com.adidtion;
+
+import java.util.Scanner;
+
+public class DaphneArray {
+	 public static void main(String[] args) 
+	    {
+	        int n, sum = 0;
+	        Scanner s = new Scanner(System.in);
+	        System.out.print("Enter no. of elements you want in array:");
+	        n = s.nextInt();
+	        int a[] = new int[n];
+	        System.out.println("Enter all the elements:");
+	        for(int i = 0; i < n; i++)
+	        {
+	            a[i] = s.nextInt();
+	            }
+	       
+	    }
+}
---
+++ 25febday21/controller/EmployeeController.java
@@ -0,0 +1,41 @@
+package com.cts.training.controller;
+import java.util.*;
+import java.util.List;
+
+import com.cts.training.bean.Employee;
+import com.cts.training.dao.EmployeeDAO;
+import com.cts.training.dao.impl.EmployeeDAOImpl;
+;
+
+public class EmployeeController {
+
+	public static void main(String[] args) {
+		EmployeeDAO empObj = new EmployeeDAOImpl();
+		Employee emp1=empObj.getEmployeeById(10);
+		emp1.setName("sanju");
+		empObj.updateEmployee(emp1);
+		//empObj.updateEmployee(empObj.getEmployeeById(10));
+		// empObj.deleteEmployee(empObj.getEmployeeById(100));
+		// System.out.println(emp);
+		 
+		//Employee employee=new Employee(100, "anuj", "Bhopal", 22, 46566);
+		//empObj.saveEmployee(employee);
+//		List<Employee> list = empObj.getAllEmployees();
+//			list.forEach(System.out::println);
+		
+//		Scanner input =new Scanner(System.in);
+//				int choice=input.nextInt();
+		//Employee emp = new Employee(111, "Disha", "Bhopal", "Digital Engineer", 22, 46566.77, 46546656L);
+		//empObj.addEmployee(emp);
+	//System.out.println( empObj.getEmployeeById(2));
+		//empObj.deleteEmployee( empObj.getEmployeeById(111));
+		
+		//Employee em=empObj.getEmployeeById(9987);
+		//em.setName("sanjay");
+		//empObj.updateEmployee(em);
+		//List<Employee> list = empObj.getAllEmployees();
+		//list.forEach(System.out::println);
+	}
+
+}
+

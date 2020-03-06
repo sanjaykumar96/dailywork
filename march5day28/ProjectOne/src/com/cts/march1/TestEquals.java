8f0f90e584f51106ce1937e120c9387c06f54fda
sanjay kumar patel <psanjaypatel2@gmail.com>
2020-03-05 18:00:23 -0800

    march5day5_loginformservlet

---
+++ march5day28/ProjectOne/src/com/cts/march1/TestEquals.java
@@ -0,0 +1,20 @@
+package com.cts.march1;
+
+public class TestEquals {
+//	public static void main(String[] args) {
+//		String str1="java";
+//		String str2=new String("java");
+//		
+//		if(str1.equals(str2)) {
+//			System.out.println("equal");
+//		}
+//		else {System.out.println("not equals");
+//	}
+	
+//	public static void main(String[] args)  {
+//		System.out.println("hi");
+//		Thread.sleep(34);
+	}
+
+}
+
